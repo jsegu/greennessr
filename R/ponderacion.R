f61ce77ce99168f0e20b41533d8113f29c6dd17e
Jordi Segú Tell <jordi.segu@isciii.es>
2020-03-05 17:13:36 +0100

    change column name

--- R/ponderacion.R
+++ R/ponderacion.R
@@ -7,10 +7,10 @@ ponderar <- function(x, urban, ndvi){
                   ifelse(x<(quantile(x,0.75)),'3','4'
                   )))}
   #indice ponderado
-  x$ponderado <- (x[,c(urban)]+((x[,c(ndvi)]+1)*50*(cor(x[,c(ndvi)], x[,c(urban)])+1)/2))/2
+  x$greenness <- (x[,c(urban)]+((x[,c(ndvi)]+1)*50*(cor(x[,c(ndvi)], x[,c(urban)])+1)/2))/2
 
   ## crear categorias
-  x$green_cat <- a(x$ponderado)
+  x$green_cat <- a(x$greenness)
 
   return(x)
 }
