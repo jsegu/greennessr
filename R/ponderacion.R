51b9db90a300e212ccd1d45707d4ea50360bddfb
Jordi Segú Tell <jordi.segu@isciii.es>
2020-03-06 12:42:59 +0100

    Cambio ponderación ndvi

--- R/ponderacion.R
+++ R/ponderacion.R
@@ -9,7 +9,7 @@ ponderar <- function(x, urban, ndvi){
                   ifelse(x<(quantile(x,0.75)),'3','4'
                   )))}
   #indice ponderado
-  res$greenness <- (x[,c(urban)]+((x[,c(ndvi)]+1)*50*(cor(x[,c(ndvi)], x[,c(urban)])+1)/2))/2
+  res$greenness <- (x[,c(urban)]+((x[,c(ndvi)]+1)*16.333*(cor(x[,c(ndvi)], x[,c(urban)])+1)/2))/2
 
   ## crear categorias
   res$green_cat <- a(res$greenness)
